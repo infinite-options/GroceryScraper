ba8b704882cd152e2b023aa74095e0661c29115b
Junha Park <jpark3@scu.edu>
2019-09-07 17:16:48 -0700

    Beginning to add more grocery stores

--- grocerycomparer.py
+++ grocerycomparer.py
@@ -4,19 +4,30 @@ import requests
 app = Flask(__name__)
 
 # URL
-wholefoods_url = 'https://products.wholefoodsmarket.com/api/search?sort=relevance&store=10259&skip=0&filters=%5B%7B%22ns%22%3A%22subcategory%22%2C%22key%22%3A%22fresh-fruit%22%2C%22value%22%3A%22produce.fresh-fruit%22%7D%5D'
+wholefoods_url = 'https://products.wholefoodsmarket.com/api/search?sort=relevance&store=10259&skip=0&filters=%5B%7B%22ns%22%3A%22category%22%2C%22key%22%3A%22produce%22%2C%22value%22%3A%22produce%22%7D%2C%7B%22ns%22%3A%22subcategory%22%2C%22key%22%3A%22fresh-vegetables%22%2C%22value%22%3A%22produce.fresh-vegetables%22%7D%5D'
+# raleys_url = 'https://shop.raleys.com/api/v2/store_products?_nocache=1567892729784&category_id=2&category_ids=2&limit=60&offset=0&sort=popular'
 
 # Request URL and parse JSON
-response = requests.get(wholefoods_url)
-response.raise_for_status() # Raise exception if response invalid
-item = response.json()['list'][0]['name']
-price = response.json()['list'][0]['store']['price']
+response_wholefoods = requests.get(wholefoods_url)
+response_wholefoods.raise_for_status() # Raise exception if response invalid
+# response_raleys = requests.get(raleys_url)
+# response_raleys.raise_for_status()
+
+# Initialize items and prices lists
+items_wholefoods = ()
+# items_raleys = ()
+# prices = []
+
+# Add items and prices
+for i in range(20):
+    items_wholefoods = items_wholefoods + ((response_wholefoods.json()['list'][i]['name']),(response_wholefoods.json()['list'][i]['store']['price']))
+    # items_raleys = items_raleys + ((response_raleys.json()['list'][i]['name']),(response_raleys.json()['list'][i]['store']['price']))
 
 # Home page routing
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', item=item, price=price)
+    return render_template('home.html', items_wholefoods=items_wholefoods)
 
 # Enable debugging when running
 if __name__ == '__main__':
