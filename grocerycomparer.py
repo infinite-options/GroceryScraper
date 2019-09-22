e3d2da47a383f97eb62cc0797958c080a0319e4f
Junha Park <jpark3@scu.edu>
2019-09-22 08:44:31 -0700

    reSort function added

--- grocerycomparer.py
+++ grocerycomparer.py
@@ -3,7 +3,7 @@ import requests
 import urllib.request
 import time
 import json
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -28,6 +28,7 @@ items = []
 prices = []
 stores = []
 units = []
+items_generic = ['Broccoli', 'Green Asparagus', 'Iceberg Lettuce', 'Cucumbers', 'Vine Tomatoes']
 
 # Convert to price per pound
 def convertPricePerUnits(basePrice, baseUnit, numberOfUnits):
@@ -49,17 +50,32 @@ def unitQuantity(productName, baseUnits):
     if baseUnits in wordsArray[1:]:
         return wordsArray[wordsArray.index(baseUnits)-1]
 
+# EXACT WORD CHECK (does not yet check tomato vs tomatoes)
+# Solves in inefficient time
+def reSort(items_generic, items, prices, storenames, units):
+    retTable = []
+    for item in items_generic:
+        itemGenArr = item.lower().split()
+        for store in storenames:
+            tableRow = []
+            for count in len(items):
+                itemArr = items[count].lower().split()
+                if all(word in itemArr for word in itemGenArr):
+                    tableRow.append(prices[count])
+                else:
+                    tableRow.append("n/a")
+            retTable.append(tableRow)
+
 # Add items and prices
 for i in range(20):
-#   items.append(responses[0].json()['list'][i]['name'])
-#   prices.append(responses[0].json()['list'][i]['store']['price'])
-#   stores.append('Whole Foods')
-#   units.append(responses[0].json()['list'][i]['store']['retail_unit'])
-#   items.append(responses[1].json()['search_response']['items']['Item'][i]['title'])
-#   prices.append(responses[1].json()['search_response']['items']['Item'][i]['price']['current_retail'])
-#   stores.append('Target')
-#   units.append('n/a')
-    
+    items.append(responses[0].json()['list'][i]['name'])
+    prices.append(responses[0].json()['list'][i]['store']['price'])
+    stores.append('Whole Foods')
+    units.append(responses[0].json()['list'][i]['store']['retail_unit'])
+    items.append(responses[1].json()['search_response']['items']['Item'][i]['title'])
+    prices.append(responses[1].json()['search_response']['items']['Item'][i]['price']['current_retail'])
+    stores.append('Target')
+    units.append('n/a')
     items.append(responses[2].json()['productsinfo'][i]['description'])
     prices.append(convertPricePerUnits(responses[2].json()['productsinfo'][i]['pricePer'],responses[2].json()['productsinfo'][i]['unitOfMeasure'],1))
     stores.append('Safeway')
@@ -68,6 +84,7 @@ for i in range(20):
 # Home page routing
 @app.route("/")
 @app.route("/home")
+
 def home():
     return render_template('home.html', groceries=zip(items, prices, stores, units))
 
